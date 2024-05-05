3d1d533fe228cd1d9beafb3169371dc6b5496f51
AyushShukla24 <ayush.shukla.as.2003@gmail.com>
2024-05-05 19:37:45 +0530

    third change

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,6 +16,7 @@ export const updateUser=async(req,res)=>{
 
 export const deleteUser=async(req,res)=>{
     const id=req.params.id;
+    console.log(id)
 
     try{
         const deletedUser =await UserSchema.findByIdAndDelete(id)
